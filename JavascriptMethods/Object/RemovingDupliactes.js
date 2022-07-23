9fbb031dd4e4fe19d15864b421807db815789ce9
vijay <90137500+bijjamvijay@users.noreply.github.com>
2022-07-23 13:17:18 +0530

    Update RemovingDupliactes.js

--- JavascriptMethods/Object/RemovingDupliactes.js
+++ JavascriptMethods/Object/RemovingDupliactes.js
@@ -4,3 +4,28 @@ const ids = arr.map(o => o.id)
 const filtered = arr.filter(({id}, index) => !ids.includes(id, index + 1))
 
 console.log(filtered)
+
+
+// duplicates//
+
+let Names1 = [
+   {pen:"red"},
+   {pen:"red"},
+   {pen:"black"},
+   {pen:"green"},
+   {pen:"blue"},    //black,green,blue
+];
+
+// Creates an array of objects with unique "name" property values.
+let Array = [
+    ...new Map(Names1.map((a) => [a["pen"], a])).values(),
+];
+
+console.log(Array); 
+
+// 2nd method
+let  students = [{name:"vijay"},{name:"ajay"},{name:"murali"},{name:"vijay"},
+  
+  ]
+  const uniqueArr = [... new Set(students.map((data) => data.name))]
+  console.log(uniqueArr)
